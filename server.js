97037b820d8266c48d2bcd5719289a31043f7cb7
vaishnav soni <vaishnav.soni@adelantelabs.com>
2019-01-24 17:48:21 +0530

    project setup

---
+++ server.js
@@ -0,0 +1,44 @@
+var express = require('express')
+var app = express();
+var server = require('http').createServer(app)
+var io = require('socket.io').listen(server)
+var users = [];
+var connections = [];
+
+server.listen(process.env.port || 3000)
+console.log('Server running...');
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname = 'C:\Users\vaishnav-atl\Desktop\Weekly Assignment\Weekly Assignment 1/index.html')
+});
+
+io.sockets.on('connection', function (socket) {
+    connections.push(socket);
+    console.log('Connected: %s sockets connected', connections.length)
+
+    //Disconnect
+    socket.on('disconnect', function (data) {
+        if (!socket.username) return;
+        users.splice(users.indexOf(socket.username), 1)
+        updateUsernames();
+        connections.splice(connections.indexOf(socket), 1)
+        console.log('Disconnected: %s sockets connected', connections.length)
+    });
+
+    //Send Mesasge 
+    socket.on('send message', function (data) {
+        console.log(data)
+        io.sockets.emit('new message', { msg: data, user: socket.username });
+    });
+
+    //New user 
+    socket.on('new user', function (data, callback) {
+        callback(true);
+        socket.username = data;
+        users.push(socket.username);
+        updateUsernames();
+    });
+    function updateUsernames() {
+        io.sockets.emit('get users', users);
+    }
+});
